2a9cbaa402333d02f79821d18ae85ae1ffed8e74
changjiang <changjiang@opera.com>
2015-04-29 18:20:58 +0800

    template & view

--- matrix/settings.py
+++ matrix/settings.py
@@ -1,4 +1,11 @@
 # Django settings for matrix project.
+import sys
+import os.path
+
+
+PROJECT_ROOT = os.path.join(
+    os.path.realpath(os.path.dirname(__file__)), os.pardir)
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -111,6 +118,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates/'),
 )
 
 INSTALLED_APPS = (
@@ -121,7 +129,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- matrix/urls.py
+++ matrix/urls.py
@@ -1,10 +1,14 @@
-from django.conf.urls import patterns, include, url
+# coding:utf-8
+
+from django.conf.urls import *
+from matrix.view import hello
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    ('^hello/$', hello),
     # Examples:
     # url(r'^$', 'matrix.views.home', name='home'),
     # url(r'^matrix/', include('matrix.foo.urls')),
@@ -13,5 +17,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
